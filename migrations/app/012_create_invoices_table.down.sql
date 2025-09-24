c75b97a22018d985aeba78761bb630a4fc4dde9e
IlmanNafi11 <studyilmannafi@gmail.com>
2025-09-24 10:42:15 +0700

    Fixed: 1. Incompatible MySQL syntax error in PostgreSQL migration file.

--- migrations/app/012_create_invoices_table.down.sql
+++ migrations/app/012_create_invoices_table.down.sql
@@ -1,7 +1,8 @@
-DROP TRIGGER IF EXISTS invoices_updated_at;
-DROP INDEX IF EXISTS idx_invoices_created_at ON invoices;
-DROP INDEX IF EXISTS idx_invoices_subscription_plan_id ON invoices;
-DROP INDEX IF EXISTS idx_invoices_dibayar_pada ON invoices;
-DROP INDEX IF EXISTS idx_invoices_status ON invoices;
-DROP INDEX IF EXISTS idx_invoices_user_id ON invoices;
+DROP TRIGGER IF EXISTS trigger_invoices_updated_at ON invoices;
+DROP FUNCTION IF EXISTS update_invoices_updated_at();
+DROP INDEX IF EXISTS idx_invoices_created_at;
+DROP INDEX IF EXISTS idx_invoices_subscription_plan_id;
+DROP INDEX IF EXISTS idx_invoices_dibayar_pada;
+DROP INDEX IF EXISTS idx_invoices_status;
+DROP INDEX IF EXISTS idx_invoices_user_id;
 DROP TABLE IF EXISTS invoices;
